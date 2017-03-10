d4df9fe3c4b4a9c828668d43c466b5c4f9edad76
jkydevelopment <jky.development@gmail.com>
2017-03-10 12:55:30 +0100

    Creación del proyecto

---
+++ MetodologiaAgil.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodologiaagil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Carlos
+ */
+public class MetodologiaAgil {
+
+    Scanner teclado = new Scanner (System.in);
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        startCalculadora();
+    }
+    
+    void startCalculadora(){
+        while(true){
+            System.out.println("¿Qué operación quieres realizar?");
+            System.out.println("1: Sumar");
+            System.out.println("2: Restar");
+            System.out.println("3: Multiplicar");
+            String cadena;
+            cadena = teclado.nextLine();
+            
+            switch (cadena){
+                case "1": 
+                    System.out.println("El resultado de la suma es: " + suma());
+                    break;
+                case "2": 
+                    System.out.println("El resultado de la resta es: " + resta());
+                    break;
+                case "3": 
+                    System.out.println("El resultado de la suma es: " + multiplica());
+                    break;
+            }
+            
+        }
+    }
+    
+    
+    
+}
